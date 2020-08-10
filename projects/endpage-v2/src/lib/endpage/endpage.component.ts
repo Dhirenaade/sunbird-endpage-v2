e6751efe1704f70b1f04a2873d5f80a150c8114a
dhirenaade <dhirenaade@gmail.com>
2020-08-11 01:11:16 +0530

    Added Pdf Consumption Time in hours:min:seconds.

--- projects/endpage-v2/src/lib/endpage/endpage.component.ts
+++ projects/endpage-v2/src/lib/endpage/endpage.component.ts
@@ -11,22 +11,38 @@ export class EndPageComponent implements OnInit {
    // Import and export for end-page Library
    @Input() endPageConfig: object;
    @Input() contentDetails: object;
-   @Input() pdfEndData: object;
+   @Input() pdfEndData: object | any;
    // Output from EndPage
    @Output() sendMetadata: EventEmitter<object> = new EventEmitter<EndPageEvent>();
 
-   TotalTimeConsumed: number;
-   telemetryEventObject: EndPageEvent;
+   TotalTimeConsumed = 0;
+   TotalTimeConsumedInHours = '';
+   TelemetryEventObject: EndPageEvent;
    userName: any;
 
   constructor() { }
 
   ngOnInit() {
+    this.getTimeSpent();
     this.setEndPageEvent('END');
   }
 
   setEndPageEvent( eid: string ) {
-    this.sendMetadata.emit(this.telemetryEventObject);
+    this.sendMetadata.emit(this.TelemetryEventObject);
+  }
+
+  getTimeSpent() {
+    for ( const eachPageTime of this.pdfEndData['metaData']['pageDuration']) {
+      this.TotalTimeConsumed += (eachPageTime.spentTime);
+    }
+   this.TotalTimeConsumedInHours = this.convertMilisecondToMinutes(this.TotalTimeConsumed);
+  }
+
+  convertMilisecondToMinutes( duration: number) {
+    const seconds = Math.floor((duration / 1000) % 60);
+    const minutes = Math.floor(((duration / (1000 * 60 ) ) % 60));
+    const hours = Math.floor((duration / (1000 * 60 * 60)) % 24);
+    return hours + ':' + minutes + ':' + seconds;
   }
 
   replay(): void {
