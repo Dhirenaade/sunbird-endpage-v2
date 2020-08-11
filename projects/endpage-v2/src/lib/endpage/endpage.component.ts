0f28d38ea9def9ae7e26e9369cba5dd7a8343e19
Rahul Shukla <rahul.shukla13@gmail.com>
2020-08-11 11:41:03 +0530

    UI fixes

--- projects/endpage-v2/src/lib/endpage/endpage.component.ts
+++ projects/endpage-v2/src/lib/endpage/endpage.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, Output, Input, EventEmitter, OnDestroy } from '@angular/core';
 
-import { EndPageEvent } from './endPageEvents';
+import { EndPageEvent, ContentDetails, EndPageConfig } from './endPageEvents';
 
 @Component({
   selector: 'sb-lib-endpage',
@@ -9,31 +9,52 @@ import { EndPageEvent } from './endPageEvents';
 })
 export class EndPageComponent implements OnInit {
    // Import and export for end-page Library
-   @Input() endPageConfig: object;
-   @Input() contentDetails: object;
-   @Input() pdfEndData: object;
+   @Input() endPageConfig: EndPageConfig;
+   @Input() contentDetails: ContentDetails;
+   @Input() pdfEndData: object | any;
    // Output from EndPage
    @Output() sendMetadata: EventEmitter<object> = new EventEmitter<EndPageEvent>();
+   @Output() replayEvent: EventEmitter<string> = new EventEmitter<string>();
+   @Output() exitEvent: EventEmitter<string> = new EventEmitter<string>();
 
-   TotalTimeConsumed: number;
-   telemetryEventObject: EndPageEvent;
+   TotalTimeConsumed = 0;
+   TotalTimeConsumedInHours = '';
+   TelemetryEventObject: EndPageEvent;
+   userName: any;
 
   constructor() { }
 
   ngOnInit() {
+    this.getTimeSpent();
     this.setEndPageEvent('END');
   }
 
   setEndPageEvent( eid: string ) {
-    this.sendMetadata.emit(this.telemetryEventObject);
+    this.sendMetadata.emit(this.TelemetryEventObject);
   }
 
-  replay(): void {
+  getTimeSpent() {
+    console.log(this.pdfEndData);
+    for ( const eachPageTime of this.pdfEndData['metaData']['pageDuration']) {
+      this.TotalTimeConsumed += (eachPageTime.spentTime);
+    }
+   this.TotalTimeConsumedInHours = this.convertMilisecondToMinutes(this.TotalTimeConsumed);
+  }
+
+  convertMilisecondToMinutes( duration: number) {
+    const seconds = Math.floor((duration / 1000) % 60);
+    const minutes = Math.floor(((duration / (1000 * 60 ) ) % 60));
+    const hours = Math.floor((duration / (1000 * 60 * 60)) % 24);
+    return hours + ':' + minutes + ':' + seconds;
+  }
+
+  replay(action): void {
+    this.replayEvent.emit(action);
 
   }
 
   exit(): void {
-
+    this.replayEvent.emit('exit');
   }
 
 }
